3a5f7f47d04beb631bec5de1ff7a84c44943ab13
florin <florinutz@gmail.com>
2020-04-13 19:43:17 +0200

    actually doing smth

--- config.js
+++ config.js
@@ -1,19 +1,43 @@
 module.exports = {
-  url: 'https://1337x.to/popular-movies',
-  urls: [],
-  requestOptions: {
-    timeout: 10000,
-    gzip: true
-  },
-  beforeRequest: function() { return Promise.resolve({}) },
-  afterProcessed: function(res) { return res },
-  debugRequest: false,
-  randomUserAgent: true,
-  printRequestUrl: false,
-  promiseLimit: 3,
-  randomWait: 5,
-  process: function({ $, url, error, createdAt }) {
-    if (error) throw Error(error)
-    return $('.coll-1.name a:nth-child(2)').map((i, el) => console.log($(el).text()))
-  },
+    // url: 'https://1337x.to/popular-movies',
+    urls: [
+        'https://1337x.to/search/movies/8/',
+        'https://1337x.to/search/movies/9/',
+        'https://1337x.to/search/movies/10/',
+        'https://1337x.to/search/movies/11/',
+        'https://1337x.to/search/movies/12/',
+        'https://1337x.to/search/movies/13/',
+        'https://1337x.to/search/movies/14/',
+    ],
+    requestOptions: {
+        timeout: 10000,
+        gzip: true
+    },
+    beforeRequest: function () {
+        return Promise.resolve({})
+    },
+    afterProcessed: function (res) {
+        return res
+    },
+    debugRequest: false,
+    randomUserAgent: true,
+    printRequestUrl: false,
+    promiseLimit: 3,
+    randomWait: 5,
+    process: function ({$, url, error, createdAt}) {
+        if (error) throw Error(error)
+        return $('.coll-1.name a:nth-child(2)').map((i, el) => console.log($(el).text()))
+    },
+    next: {
+        key: '',
+        process: function ({$, url, error, createdAt, prevRes}) {
+            if (error) throw Error(error)
+            throw Error('Missing implementation next')
+        }
+    },
+    finally: function (res, _) {
+    },
+    catch: function (err) {
+        console.error(err)
+    }
 }
